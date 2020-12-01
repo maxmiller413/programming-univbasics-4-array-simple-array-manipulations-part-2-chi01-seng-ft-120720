6e3b92aeb0dbeab67cf3cc3558720659a204423f
Max Miller <maxmiller413@gmail.com>
2020-12-01 13:28:37 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -2,3 +2,10 @@ def using_concat(array, second)
   array.concat(second)
 end 
 
+def using_insert(array, element)
+  array.insert(element)
+end
+
+def using_uniq(array)
+  array.uniq(duplicates)
+end 
